da245998f83262c65e4689240a6c0db34f431d44
Huynh Luong Nhut Toan <luongnhuttoan.huynh@concentrix.com>
2022-12-05 16:29:16 +0700

    medusa admin new url

--- admin/src/services/config.js
+++ admin/src/services/config.js
@@ -1,3 +1,3 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://abbott-app-production.up.railway.app/"
 
 export { medusaUrl }
