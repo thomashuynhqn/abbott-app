d1aa68c99b2a888421b215a5a286d75a84ac4e7f
Huynh Luong Nhut Toan <luongnhuttoan.huynh@concentrix.com>
2022-12-12 15:20:07 +0700

    try to not connect sqlitesqlite

--- server/medusa-config.js
+++ server/medusa-config.js
@@ -71,8 +71,6 @@ module.exports = {
     // For more production-like environment install PostgresQL
     database_url: DATABASE_URL,
     database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
   },
